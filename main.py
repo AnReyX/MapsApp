ad7a59118600392b4f6b6943a148ea12c11a25de
Mega_Axolotl <85391186+Vlod-and-whot@users.noreply.github.com>
2024-02-02 19:48:15 +0200

    Add files via upload
    Task1_True

--- main.py
+++ main.py
@@ -2,20 +2,30 @@ import sys
 import requests
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QTableWidgetItem, QHeaderView, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class MapsAPI(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('Map_API.ui', self)
-        self.request = "https://static-maps.yandex.ru/1.x/?ll=139.295036%2C-24.682020&spn=0.016457,20.91619&l=sat"
-        self.response = requests.get(self.request)
         self.map_file = 'map.png'
+
+        self.pushButton.clicked.connect(self.updateMap)
+
+    def updateMap(self):
+        x_position = self.X_Pos_line.text()
+        y_position = self.Y_Pos_line.text()
+        zoom = self.Zoom_line.text()
+
+        self.request = f"https://static-maps.yandex.ru/1.x/?ll={x_position},{y_position}&z={zoom}&l=sat"
+        response = requests.get(self.request)
+
+        # Обновляем карту
         with open(self.map_file, 'wb') as f:
-            f.write(self.response.content)
-        self.pixmap = QPixmap(self.map_file)
-        self.Result_picture_label.setPixmap(self.pixmap)
+            f.write(response.content)
+        pixmap = QPixmap(self.map_file)
+        self.Result_picture_label.setPixmap(pixmap)
 
 
 if __name__ == '__main__':
